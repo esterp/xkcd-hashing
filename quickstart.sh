a7d57993a9d534ca7fca3230827afe4674c14376
Yasyf Mohamedali <yasyf@yasyf.com>
2013-04-01 23:01:51 -0700

    brew instead of apt-get if on OS X

--- quickstart.sh
+++ quickstart.sh
@@ -5,11 +5,17 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-apt-get install build-essential
-apt-get install python3
-apt-get install python3-dev
-apt-get install python-virtualenv
-apt-get install libgmp3-dev
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+	apt-get install build-essential
+	apt-get install python3
+	apt-get install python3-dev
+	apt-get install python-virtualenv
+	apt-get install libgmp3-dev
+elif [[ "$unamestr" == 'Darwin' ]]; then
+   brew install python3
+   brew intall gmp
+fi
 
 virtualenv venv --python=python3
 source venv/bin/activate
