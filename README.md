5b095060e2599f774b2aed1f27d360ea17778294
Dalton Hubble <dghubble@gmail.com>
2013-04-02 02:03:01 -0400

    Added otheros.py for easier usage by Windows/OSX users.

--- README.md
+++ README.md
@@ -2,14 +2,24 @@
 
 Search for data that hashes to the correct goal output hash. This script will randomly Skein1024 hash data and check the digest output to determine how many bits are different from the goal output. Whenever a "best" guess is found, it is submitted to the xkcd site, assuming your network connection is working.
 
-Note: Quickstart script uses apt-get.
 
-# Quickstart
+# Quickstart 
+
+## Ubuntu
 
 	sudo ./quickstart.sh
 	source venv/bin/activate
 
     python3 xkcd-hashing.py
+
+    Note: Quickstart script uses apt-get.
+
+## Windows/OS X
+
+	Be sure you have Python 3 and pyskein. This version does not use gmpy so it may be easier for non-Linux users to setup. Note that its performance is not as great though.
+
+	python3 otheros.py
+
 
 Wait and hope you find a guess with a small diff (i.e. closer to the goal hash).
 
---
+++ otheros.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+import multiprocessing, signal, time, skein, random, string, urllib.request, urllib.parse
+
+TARGETSTR = '5b4da95f5fa08280fc9879df44f418c8f9f12ba424b7757de02bbdfbae0d4c4fd' + \
+	'f9317c80cc5fe04c6429073466cf29706b8c25999ddd2f6540d4475cc977b87f4757be' + \
+	'023f19b8f4035d7722886b78869826de916a79cf9c94cc79cd4347d24b567aa3e2390' + \
+	'a573a373a48a5e676640c79cc70197e1c5e7f902fb53ca1858b6'
+
+TARGET = int(TARGETSTR, 16)
+
+RANDOM_BIT_LEN = 512
+
+def init_worker():
+	signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+def submit(word):
+	url = "http://almamater.xkcd.com/?edu=mit.edu"
+	data = urllib.parse.urlencode({'hashable': word})
+	binarydata = data.encode('ascii')
+	urllib.request.urlopen(url, binarydata)
+		
+def run_worker(do_submit=True, time_limit=None):
+	best = float('inf')
+	r = random.SystemRandom()
+	t = time.time()
+	i = 0
+	while True:
+		# guess = random.getrandbits(RANDOM_BIT_LEN)
+
+		# encoded = gmpy.digits(guess, 62).encode('ascii')
+		# digest = gmpy.mpz(skein.skein1024(encoded).digest()[::-1] + b'\0', 256)
+		# diff = gmpy.hamdist(digest, TARGET)
+
+		guess = hex(r.getrandbits(RANDOM_BIT_LEN))[2:]
+		encoded = guess.encode('utf-8')
+		digest = int(skein.skein1024(encoded).hexdigest(), 16)
+		diff = bin(digest ^ TARGET).count('1')
+		if diff < best:
+			best = diff
+			if do_submit:
+				submit(guess)
+			print('Found new best input with diff [%.3d]: \"%s\"' %
+				(diff, guess))
+		i += 1
+		if time_limit and time.time() - t > time_limit:
+			break
+	return i
+
+def main():
+	run_worker()
+	cpus = multiprocessing.cpu_count()
+	pool = multiprocessing.Pool(cpus, init_worker)
+	for i in range(cpus):
+		pool.apply_async(run_worker)
+	try:
+		while True:
+			time.sleep(100)
+	except KeyboardInterrupt:
+		print('Terminating...')
+		pool.terminate()
+		pool.join()
+	else:
+		print('Quitting...')
+		pool.close()
+		pool.join()
+
+if __name__ == '__main__':
+	import sys
+	if len(sys.argv) > 1 and sys.argv[1] == 'time':
+		iters = run_worker(do_submit=False, time_limit=1)
+		print('Processed', iters, 'guesses in 1 second')
+	else:
+		main()
